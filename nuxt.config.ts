1db05f572788c05368c7e9d8fe73dc4fb814abd4
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-08-15 11:27:02 +0330

    env file configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      APP_NAME: process.env.APP_NAME,
+      APP_LOCALE: process.env.APP_LOCALE,
+      APP_DEBUG: process.env.APP_DEBUG?.toLowerCase() === "true" || process.env.APP_DEBUG == "1",
+      APP_ENV: process.env.APP_ENV ?? 'debug',
+    }
+  }
 })
