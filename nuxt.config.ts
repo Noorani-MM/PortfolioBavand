39153bbe9675e926bdb996071f8a1cd6e394ee65
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-08-15 11:28:59 +0330

    Tailwind css installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
       APP_NAME: process.env.APP_NAME,
@@ -9,5 +10,11 @@ export default defineNuxtConfig({
       APP_DEBUG: process.env.APP_DEBUG?.toLowerCase() === "true" || process.env.APP_DEBUG == "1",
       APP_ENV: process.env.APP_ENV ?? 'debug',
     }
-  }
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
